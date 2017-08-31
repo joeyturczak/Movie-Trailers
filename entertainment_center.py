3771856c174b6cc7cb78941a40331d918a1b2954
Joey Turczak <joeyturczak@gmail.com>
2017-08-31 10:01:27 -0700

    added more movie data

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,5 +5,13 @@ memento = media.Movie("Memento",
                       "https://upload.wikimedia.org/wikipedia/en/c/c7/Memento_poster.jpg",
                       "https://www.youtube.com/watch?v=UFuFFdK7i44")
 
-movies = [memento]
+speed_racer = media.Movie("Speed Racer",
+                          "https://upload.wikimedia.org/wikipedia/en/8/82/Speed_racer_ver5_xlg.jpg",
+                          "https://www.youtube.com/watch?v=8V8sLlqJB2w")
+
+princess_mononoke = media.Movie("Princess Mononoke",
+                                "https://upload.wikimedia.org/wikipedia/en/2/24/Princess_Mononoke_Japanese_Poster_%28Movie%29.jpg",
+                                "https://www.youtube.com/watch?v=4OiMOHRDs14")
+
+movies = [memento, speed_racer, princess_mononoke]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,7 +1,7 @@
-class Movies()
+class Movie():
     """ This class stores movie information """
 
-    def __init__(self, title, poster_image_url, trailer_url)
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
         self.poster_image_url = poster_image_url
-        self.trailer_url = trailer_url
+        self.trailer_youtube_url = trailer_youtube_url
