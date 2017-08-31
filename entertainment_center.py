874fd31c1506d48c97ebe2c65ec1d9bb1626fd25
Joey Turczak <joeyturczak@gmail.com>
2017-08-31 10:05:05 -0700

    added comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,7 @@
 import media
 import fresh_tomatoes
 
+# Movie data
 memento = media.Movie("Memento",
                       "https://upload.wikimedia.org/wikipedia/en/c/c7/Memento_poster.jpg",
                       "https://www.youtube.com/watch?v=UFuFFdK7i44")
@@ -13,5 +14,8 @@ princess_mononoke = media.Movie("Princess Mononoke",
                                 "https://upload.wikimedia.org/wikipedia/en/2/24/Princess_Mononoke_Japanese_Poster_%28Movie%29.jpg",
                                 "https://www.youtube.com/watch?v=4OiMOHRDs14")
 
+# Create an array of movie data
 movies = [memento, speed_racer, princess_mononoke]
+
+# Open browser with movie data from array
 fresh_tomatoes.open_movies_page(movies)
